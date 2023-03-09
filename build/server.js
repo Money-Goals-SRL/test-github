a1686cd5aeed260fea52579b946b741238b8c6de
leonardopinna <96293968+leonardopinna@users.noreply.github.com>
2023-03-09 15:17:12 +0100

    Changed gitignore file to include build

--- build/server.js
+++ build/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/build"));
+app.use(express.static("build"));
 
 app.get("/*", function (req, res) {
   res.sendFile(__dirname + "/build/index.html");
